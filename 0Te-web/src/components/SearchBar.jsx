70bc44f15a81a9fe242f6732e668577688a96225
0TeBrick <mauricioalessandro@yahoo.com>
2025-05-27 19:45:15 +0200

    quitamos el hover del X

--- 0Te-web/src/components/SearchBar.jsx
+++ 0Te-web/src/components/SearchBar.jsx
@@ -45,7 +45,7 @@ export default function SearchBar({ initialQuery = "" }) {
         <button
           type="button" // Importante: type="button" para que no envíe el formulario
           onClick={handleClearSearch}
-          className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none p-1 rounded-full" // Ajusta right y otros estilos
+          className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 focus:outline-none p-1 rounded-full" // Ajusta right y otros estilos
           aria-label="Limpiar búsqueda"
         >
           <XMarkIcon className="h-5 w-5" /> {/* Usa el icono XMarkIcon */}
